b04f3fd164d70b10174cb03874f1b468aea54e60
Binary <rukywe@me.com>
2021-03-05 15:53:53 +0000

    made some minor changes

--- routes/index.js
+++ routes/index.js
@@ -20,8 +20,9 @@ router.get('/dashboard', ensureAuth, async (req, res) => {
     try {
         const stories = await Story.find({ user: req.user.id }).lean()
           res.render("dashboard", {
-              name: req.user.firstName,
-              stories
+            name: req.user.firstName,
+            picture: req.user.image,
+            stories,
           });
     } catch (error) {
         console.error(error)
